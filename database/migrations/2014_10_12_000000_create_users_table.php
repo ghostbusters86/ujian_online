4d385c945615fa146221becac640f6d5c332b937
team-linecode <linecode.team@gmail.com>
2020-06-23 14:06:03 +0700

    add fontawesome

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
@@ -19,15 +14,11 @@ class CreateUsersTable extends Migration
             $table->string('nim')->nullable();
             $table->string('username');
             $table->string('password');
+            $table->enum('level', ['admin', 'mahasiswa']);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users');
